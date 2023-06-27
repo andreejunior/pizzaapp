1124140137bbf9d5a482f7889587bc0b8166a9c7
andre <andrelpdsilvajunior9999@gmail.com>
2023-06-27 17:14:37 -0300

    27-06-23

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -1,9 +1,14 @@
 <?php
-
+/**
+ * |---------------------------------
+ * Andre Junior - 27-06-23
+ * |---------------------------------
+ */
 namespace App\Http\Controllers;
 
-use App\Models\Cargo;
 use Illuminate\Http\Request;
+use App\Models\{Cargo,User};
+use Ramsey\Uuid\Type\Integer;
 
 class CargoController extends Controller
 {
@@ -12,7 +17,9 @@ class CargoController extends Controller
      */
     public function index()
     {
-        //
+        $cargos = Cargo::orderBy('cargo');
+        return view('cargo.index')
+        ->with(compact('cargos'));
     }
 
     /**
@@ -20,7 +27,9 @@ class CargoController extends Controller
      */
     public function create()
     {
-        //
+        $cargo = null;
+        return view('cargo.form')
+        ->with(compact('cargo'));
     }
 
     /**
@@ -28,38 +37,48 @@ class CargoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $cargo = Cargo::create($request->all());
+        return redirect()->route('cargo.index')
+        ->with('success','Cadastrado com Sucesso!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Cargo $cargo)
+    public function show(integer $id)
     {
-        //
+        $cargo = Cargo::find($id);
+        return view('cargo.show')->with(compact('cargo'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Cargo $cargo)
+    public function edit(int $id)
     {
-        //
+        $cargo = Cargo::find($id);
+        return view('cargo.form')
+        ->with(compact('cargo'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Cargo $cargo)
+    public function update(Request $request, int $id)
     {
-        //
+        $cargo = Cargo::find($id);
+        $cargo->update($request->all());
+        return redirect()->route('cargo.index')
+        ->with('success','Atualizado com Sucesso!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Cargo $cargo)
+    public function destroy(int $id)
     {
-        //
+        Cargo::find($id)->delete();
+        return redirect()->back()
+        ->with('destroy','Excluido com Sucesso!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{ProfileController,CargoController,ClienteController,EnderecoController,PedidoController,ProdutoController,ProdutoTamanhoController,};
+use App\Models\Endereco;
+use App\Models\Pedido;
+use App\Models\Produto;
+use App\Models\Tamanho;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +31,84 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/**
+ * ----------------------------
+ * Cargos
+ * ----------------------------
+ */
+Route::prefix('cargos')
+     ->controller(CargoController::class)
+     ->group(function () {
+        Route::get('/','index')->name('cargo.index');
+        Route::get('/novo', 'create')->name('cargo.create');
+        Route::get('/{id}', 'show')->name('cargo.show');
+        Route::get('/editar/{id}', 'edit')->name('cargo.editar');
+        Route::post('/store', 'store')->name('cargo.store');
+        Route::post('/update', 'update')->name('cargo.update');
+        Route::post('/destroy', 'destroy')->name('cargo.destroy');
+
+     });
+
+     Route::prefix('clientes')
+     ->controller(ClienteController::class)
+     ->group(function () {
+        Route::get('/','index')->name('cliente.index');
+        Route::get('/novo', 'create')->name('cliente.create');
+        Route::get('/{id}', 'show')->name('cliente.show');
+        Route::get('/editar/{id}', 'edit')->name('cliente.editar');
+        Route::post('/store', 'store')->name('cliente.store');
+        Route::post('/update', 'update')->name('cliente.update');
+        Route::post('/destroy', 'destroy')->name('cliente.destroy');
+     });
+
+     Route::prefix('enderecos')
+     ->controller(EnderecoController::class)
+     ->group(function () {
+        Route::get('/','index')->name('enderecos.index');
+        Route::get('/novo', 'create')->name('enderecos.create');
+        Route::get('/{id}', 'show')->name('enderecos.show');
+        Route::get('/editar/{id}', 'edit')->name('enderecos.editar');
+        Route::post('/store', 'store')->name('enderecos.store');
+        Route::post('/update', 'update')->name('enderecos.update');
+        Route::post('/destroy', 'destroy')->name('enderecos.destroy');
+     });
+
+     Route::prefix('pedidos')
+     ->controller(PedidoController::class)
+     ->group(function () {
+        Route::get('/','index')->name('pedido.index');
+        Route::get('/novo', 'create')->name('pedido.create');
+        Route::get('/{id}', 'show')->name('pedido.show');
+        Route::get('/editar/{id}', 'edit')->name('pedido.editar');
+        Route::post('/store', 'store')->name('pedido.store');
+        Route::post('/update', 'update')->name('pedido.update');
+        Route::post('/destroy', 'destroy')->name('pedido.destroy');
+     });
+
+     Route::prefix('produtos')
+     ->controller(ProdutoController::class)
+     ->group(function () {
+        Route::get('/','index')->name('produto.index');
+        Route::get('/novo', 'create')->name('produto.create');
+        Route::get('/{id}', 'show')->name('produto.show');
+        Route::get('/editar/{id}', 'edit')->name('produto.editar');
+        Route::post('/store', 'store')->name('produto.store');
+        Route::post('/update', 'update')->name('produto.update');
+        Route::post('/destroy', 'destroy')->name('produto.destroy');
+     });
+
+     Route::prefix('tamanhos')
+     ->controller(ProdutoTamanhoController::class)
+     ->group(function () {
+        Route::get('/','index')->name('tamanho.index');
+        Route::get('/novo', 'create')->name('tamanho.create');
+        Route::get('/{id}', 'show')->name('tamanho.show');
+        Route::get('/editar/{id}', 'edit')->name('tamanho.editar');
+        Route::post('/store', 'store')->name('tamanho.store');
+        Route::post('/update', 'update')->name('tamanho.update');
+        Route::post('/destroy', 'destroy')->name('tamanho.destroy');
+     });
+
 
 require __DIR__.'/auth.php';
